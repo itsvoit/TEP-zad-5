97497c7dacd826ef75afcec9e66173219b380371
itsvoit <wojciech.skrzypiec2@gmail.com>
2022-10-18 19:27:26 +0200

    Finish ex1 and ex2

---
+++ CMySmartPointer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by voit on 18.10.2022.
+//
+
+#include "CMySmartPointer.h"
---
+++ CMySmartPointer.h
@@ -0,0 +1,68 @@
+//
+// Created by voit on 18.10.2022.
+//
+
+#ifndef TEP_ZAD_5_CMYSMARTPOINTER_H
+#define TEP_ZAD_5_CMYSMARTPOINTER_H
+
+class CRefCounter
+{
+public:
+	CRefCounter() { i_count; }
+	int iAdd() { return(++i_count); }
+	int iDec() { return(--i_count); };
+	int iGet() { return(i_count); }
+private:
+	int i_count;
+};//class CRefCounter
+
+
+template<typename POINTER_TYPE>
+class CMySmartPointer {
+public:
+	CMySmartPointer(POINTER_TYPE *pcPointer) {
+		pc_pointer = pcPointer;
+		pc_counter = new CRefCounter();
+		pc_counter->iAdd();
+	}//CMySmartPointer(CSellData *pcPointer)
+
+	CMySmartPointer(const CMySmartPointer<POINTER_TYPE> &pcOther) {
+		pc_pointer = pcOther.pc_pointer;
+		pc_counter = pcOther.pc_counter;
+		pc_counter->iAdd();
+	}//CMySmartPointer(const CMySmartPointer &pcOther)
+
+	~CMySmartPointer() {
+		v_cnt_decrement();
+	}//~CMySmartPointer()
+
+	POINTER_TYPE &operator*() {
+		return (*pc_pointer);
+	}
+
+	POINTER_TYPE *operator->() {
+		return (pc_pointer);
+	}
+
+	CMySmartPointer &operator=(const CMySmartPointer<POINTER_TYPE> &pcOther) {
+		v_cnt_decrement();
+		pc_pointer = pcOther.pc_pointer;
+		pc_counter = pcOther.pc_counter;
+		pc_counter->iAdd();
+
+		return (*this);
+	}
+
+private:
+	CRefCounter *pc_counter;
+	POINTER_TYPE *pc_pointer;
+
+	void v_cnt_decrement(){
+		if (pc_counter->iDec() == 0) {
+			delete pc_pointer;
+			delete pc_counter;
+		}//if (pc_counter->iDec())
+	}
+};
+
+#endif //TEP_ZAD_5_CMYSMARTPOINTER_H
